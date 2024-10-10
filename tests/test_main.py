e9a12064eb03339eaae155298fc70fc0420c3b77
Abdelhadi Alioui <abdelhadialioui2003@gmail.com>
2024-10-10 17:06:41 +0200

    ajout des tests sur main

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,16 @@
 from olympics.__main__ import main
-
+import pytest
+import argparse
 
 def test_countries():
-    argv = ['countries']
+    argv = ['countries', '--top', '10']
     main(argv)
+
+def test_collective():
+    argv = ['collective', '--top', '5']
+    main(argv)
+
+def test_individual():
+    argv = ['individual', '--top', '10']
+    main(argv)
+
