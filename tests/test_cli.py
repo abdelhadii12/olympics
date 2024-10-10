3e95522881d8aa8d1757d8c834ece47fd33548dc
Abdelhadi Alioui <abdelhadialioui2003@gmail.com>
2024-10-10 16:31:04 +0200

    Ajout des tests sur Cli

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -8,3 +8,29 @@ def test_top_countries():
     cli.top_countries(file=string)
     text = string.getvalue()
     assert 'Top' in text
+    assert 'countries' in text
+    assert 'Gold' in text
+    assert 'Silver' in text
+    assert 'Bronze' in text
+    assert 'Total' in text
+
+def test_top_collective():
+    string = StringIO()
+    cli.top_collective(file=string)
+    text = string.getvalue()
+    assert 'Top' in text
+    assert 'collective events' in text
+    assert 'Country' in text
+    assert 'Medals' in text
+
+def test_top_individual():
+    string = StringIO()
+    cli.top_individual(file=string)
+    text = string.getvalue()
+    assert 'Top' in text
+    assert 'individual events' in text
+    assert 'Name' in text
+    assert 'Gender' in text
+    assert 'Country' in text
+    assert 'Medals' in text
+
